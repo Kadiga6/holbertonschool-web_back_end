142c78bd16d38026319161d59e0e01d273f5a384
Kadiga6 <9510@holbertonstudents.com>
2025-04-27 22:59:06 +0200

    9

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """
-Ce module contient une fonction add qui prend deux floats en argument
-et retourne leur somme.
+Ce module contient une fonction add qui additionne deux nombres flottants.
 """
 
+
 def add(a: float, b: float) -> float:
-    """Retourne la somme de deux nombres flottants."""
-    return a + b
+        """Retourne la somme de deux nombres flottants."""
+        return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """
-Ce module contient une fonction concat qui prend deux chaînes de caractères 
-en argument et retourne leur concaténation.
+Ce module contient une fonction concat qui concatène deux chaînes de caractères.
 """
 
+
 def concat(str1: str, str2: str) -> str:
-    """Concatène deux chaînes de caractères."""
+    """Retourne la concaténation de deux chaînes de caractères."""
     return str1 + str2
