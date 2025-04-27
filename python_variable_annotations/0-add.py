2ada0c3af67179aca85525ca7fb305e8f1fdd370
Kadiga6 <9510@holbertonstudents.com>
2025-04-27 22:51:22 +0200

    9

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+
+"""
+Ce module contient une fonction add qui prend deux floats en argument
+et retourne leur somme.
+"""
+
 def add(a: float, b: float) -> float:
+    """Retourne la somme de deux nombres flottants."""
     return a + b
