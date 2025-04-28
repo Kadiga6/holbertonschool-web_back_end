0c1c0074341184faebb835ef827e550a564c6248
Kadiga6 <9510@holbertonstudents.com>
2025-04-28 15:25:15 +0000

    9

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Module qui définit et annote certaines variables avec des valeurs spécifiqs.
+"""
 
 a: int = 1
 pi: float = 3.14
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
+    """
+    Prend une liste de flottants et renvoie leur somme sous forme de flottant.
+    """
     return sum(input_list)
