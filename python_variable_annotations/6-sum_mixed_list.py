c1933899e708c962d7aff9adb5244ec9e81fea27
Kadiga6 <9510@holbertonstudents.com>
2025-04-30 12:24:37 +0000

    9

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""
+Il définit une fonction annotée qui retourne un tuple à partir
+d'une chaîne de caractères et d'un nombre (int ou float).
+"""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Prend une chaîne `k` et une valeur numérique `v` (int ou float),
+    et retourne un tuple contenant la chaîne `k` et le carré de `v` (en float
+    """
     return float(sum(mxd_lst))
