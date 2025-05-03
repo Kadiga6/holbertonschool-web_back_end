f665c5d441b9b6d785e4898ef811ff55d4759fc9
Kadiga6 <9510@holbertonstudents.com>
2025-05-03 22:04:12 +0000

    9

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Module qui définit une fonction retournant une fonction multiplicatrice."""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Rtrn une fonct qui multpli un nbre flottant par le multplicatr donné"""
     return lambda x: x * multiplier
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,4 +2,5 @@
 from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Retourne une liste de tuples (élémenlongueur) pour chaque élémente l'it�rable"""
     return [(i, len(i)) for i in lst]
