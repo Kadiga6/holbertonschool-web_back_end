0c65ff0dcf722d6741465e1bd4a2e264579be9ec
root <9510@holbertonstudents.com>
2025-05-03 22:30:24 +0000

    9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Module qui définit une fonction pour obtenir la longueur de chaque élément d’un itérable de séquences.
+Module qui définit une fonction pour obtenir la longueur de chaqueelemnt
 """
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Retourne une liste de tuples (élément, longueur) pour chaque élément de l’itérable."""
+    """Rtrn une liste de tuples (elemnt,longueur) pour chaquelemnt itérable."""
     return [(i, len(i)) for i in lst]
