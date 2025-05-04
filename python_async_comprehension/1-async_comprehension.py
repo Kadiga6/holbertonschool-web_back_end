1f9f9aa2f7cedefc35288aaca26a62402d147146
root <9510@holbertonstudents.com>
2025-05-04 02:19:47 +0000

    3

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
-import async_generator
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
 
+async def async_comprehension():
+    result = [i async for i in async_generator()]
+    return result
