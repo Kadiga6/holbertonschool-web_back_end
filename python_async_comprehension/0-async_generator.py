b40e36919ad6653d5f758111e3b63cf5b83a5747
root <9510@holbertonstudents.com>
2025-05-04 01:09:58 +0000

    0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,8 @@ import random
 
 
 async def async_generator():
-    for i in range(10):
+    """Coroutine qui génère 10 nombres aléatoires entreet
+    avec une pause de 1 seconde entre chaque génération."""
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
