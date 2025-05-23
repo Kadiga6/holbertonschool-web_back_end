cb172f101ecf337eefc3204b6f261359b991cc5f
root <root@ip-10-42-207-47.ec2.internal>
2025-05-23 21:01:30 +0000

    7

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
 console.log("Welcome to Holberton School, what is your name?");
 
-
-process.stdin.resume();  // Démarre la lecture de stdin
-process.stdin.setEncoding('utf8');  // Assure que l'entrée est en texte
-
+process.stdin.resume(); // Démarre la lecture de l'entrée utilisateur
+process.stdin.setEncoding('utf8'); // Définit l'encodage en UTF-8
 
 process.stdin.on('data', (input) => {
-	console.log("Your name is: " + input.trim())
+  console.log("Your name is: " + input.trim());
+  // Optionnel : process.stdin.end(); // Pour clôturer automatiquement après une saisie
 });
 
 process.stdin.on('end', () => {
-	console.log("This important software is now closing")
+  console.log("This important software is now closing");
 });
